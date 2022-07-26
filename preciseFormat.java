9a5cdecb0c6cdc80d429d07ddb162ce1e9a31b56
loge-loki <95707717+loge-loki@users.noreply.github.com>
2022-07-26 16:53:38 +0530

    Create preciseFormat.java

---
+++ preciseFormat.java
@@ -0,0 +1,57 @@
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+//User function Template for Java
+
+class Geeks{
+    
+    static void printInFormat(float a, float b){
+        float result = a/b;
+        System.out.print(result);
+        String result1 = String.format("%.3f", result);
+        System.out.print(" " + result1);
+        
+        
+        // Your code here to print upto 3 decimal places
+        
+    }    
+    
+}
+
+// { Driver Code Starts.
+
+class GFG {
+	public static void main (String[] args) {
+		
+		//taking input using class Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//taking testcases
+		int testcases = sc.nextInt();
+		
+		while(testcases-- > 0){
+		    
+		    //taking 2 variables a,b
+    		float a = sc.nextFloat();
+    		float b = sc.nextFloat();
+    		
+    		//creating an object of class Geeks
+    		Geeks g = new Geeks();
+    		
+    		//calling printInFormat() method
+    		//of class Geeks
+    		g.printInFormat(a, b);
+    		System.out.println();
+		
+		}
+		
+	}
+}
+  // } Driver Code Ends
